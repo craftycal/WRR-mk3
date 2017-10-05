07d8cb4a97dbfc45a444ebd4bfb430fb96461f91
setup_git_prefs <setup_git_proxy>
2017-10-05 20:51:47 +1300

    first

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+
+
+
+
+$( "#facebook" ).hover(function() {
+  $( "#fbHidden" ).toggle();
+    // Animation complete.
+});
+
+$( "#email" ).hover(function() {
+  $( "#emailHidden" ).toggle();
+    // Animation complete.
+});
+
+$( "#instagram" ).hover(function() {
+  $( "#instagramHidden" ).toggle();
+    // Animation complete.
+});
+
+$( "#money" ).hover(function() {
+  $( "#moneyHidden" ).toggle();
+    // Animation complete.
+});
+
+
+});
